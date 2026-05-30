e698db0e52b7fc28c195dfb39ec32e60f1af7bb6
Josue <hounhouijosue1@gmail.com>
2026-05-30 11:03:01 +0100

    feat: create handleAddToCart function

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({}); // State to keep track of added products
+    const dispatch = useDispatch();
+
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name] : true
+        }))
+    };
 
     const plantsArray = [
         {
